f87043fbfa2aa3cde811a1db6180d0cd1fd83abd
Abubakirov Vladimir <abubakirov04@mail.ru>
2024-10-02 16:20:04 +0300

    Removed unnecessary code and added new assets
    Signed-off-by: Abubakirov Vladimir <abubakirov04@mail.ru>

--- main.cpp
+++ main.cpp
@@ -9,16 +9,8 @@ int main() {
     Space space;
     BaseWindow main_window(WINDOW_NAME, space);
 
-    space.addObject(new SpaceObject("assets/earth.jpg", {800, 200}, 1     , {200, 0}));
-    space.addObject(new SpaceObject("assets/sun.jpg"  , {800, 500}, 333000, {0  , 0}));
-
-    main_window.runEventCycle();
-
-    // Sun sun({SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2});
-    // Earth earth(100, {100, 100}, sun.getPos());
-    // space.addObject(&sun);
-    // space.addObject(&earth);
-    // main_window.registerObject(&space);
+    space.addObject(new SpaceObject("assets/earth.png", {800, 200}, 1     , {200, 0}));
+    space.addObject(new SpaceObject("assets/sun.png"  , {800, 500}, 333000, {0  , 0}));
 
     main_window.runEventCycle();
 
